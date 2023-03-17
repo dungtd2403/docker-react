fdb90f2d42c0a51dacdc30b290e1d06efd1cca95
dungtd2403 <dung.td2403@gmail.com>
2023-03-17 14:59:47 +0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('không khả thi lắm đâu, Viêt Nam dùng đồ  free cơ', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
